8ea789905aa51579dbb3d5dc8ef600d1b6635c91
Elena Kanash <hird250@gmail.com>
2022-05-21 15:34:45 +0300

    feat: add second solution to task 1

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-const filePath = path.join(__dirname, 'text.txt')
+const filePath = path.join(__dirname, 'text.txt');
 /* const filePath = path.resolve(__dirname, 'text.txt') */
 
- const readStream = fs.createReadStream(filePath, 'utf-8');
+const readStream = fs.createReadStream(filePath, 'utf-8');
 readStream.on('data', chunk => console.log(chunk));
 
 /*  fs.readFile(filePath, 'utf-8', (err, data) => {
@@ -17,7 +17,7 @@ readStream.on('data', chunk => console.log(chunk));
 /*  const readStream = fs.createReadStream(filePath, 'utf-8'); // если большой файл передаем
 let data = '';
 readStream.on('data', chunk => data += chunk);
-readStream.on('end', () => console.log('End', data));
+readStream.on('end', () => console.log(data));
 readStream.on('error', error => console.log('Error', error.message));  // если возникла ошибка */
 
 
