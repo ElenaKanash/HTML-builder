15d0e37e84295ebfb6cd6dc434482e3c86b96fcb
Elena Kanash <hird250@gmail.com>
2022-05-23 14:05:13 +0300

    feat: task 4 complete

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,18 +4,17 @@ const path = require('path');
 const folderPath = path.join(__dirname, 'secret-folder');
 //console.log(folderPath); //D:\RS project\HTML-builder\03-files-in-folder
 
-
-fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
+fs.readdir(folderPath, { withFileTypes: true }, (err, files) => { // читаем содержимое папки,  withFileTypes позволяет проверить тип файла(исключить вложенные папки)
   files.forEach(file => {
 
-    const filePath = path.join(folderPath, file.name);
+    const filePath = path.join(folderPath, file.name);//пути к файлом в папке
 
-    fs.stat(filePath, (err, stats) => {
+    fs.stat(filePath, (err, stats) => { //получаем информацию о файле
       if (err) { throw err; }
-      if (stats.isFile()) {
-        stats.size;
+      if (stats.isFile()) {     // проверяем, является ли файлом
+        stats.size; // вес каждого файла в байтах
         const stat = (stats.size / 1024).toFixed(1);
-        const ext = path.extname(filePath).slice(1);
+        const ext = path.extname(filePath).slice(1); //расширение каждого файла
         console.log(file.name + ' - ' + ext + ' - ' + stat + 'kb');
       }
     });   // console.log(file);
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,43 @@
+const path = require('path');
+const fs = require('fs');
+//const fsPromises = fs.promises;
+
+async function copyDir() {
+
+  const folderPath = path.join(__dirname, 'files');
+  const copyFolderPath = path.join(__dirname, 'files-copy');
+
+  await
+
+  fs.promises.rm(copyFolderPath, { recursive: true, force: true });  // удаляем копи-папку с вложенными файлами
+
+  fs.mkdir(copyFolderPath, { recursive: true }, err => { //cоздаем копи-папку
+    if (err) throw err;
+  });
+
+  fs.readdir(folderPath, { withFileTypes: true }, (err, files) => { // читаем содержимое папки
+    if (err) throw err;
+
+    files.forEach(file => {
+      const filePath = path.join(__dirname, 'files', file.name);//пути к файлам в папке и копи-папке
+      const copyFilePath = path.join(__dirname, 'files-copy', file.name);
+
+      /* fs.copyFile(filePath, copyFilePath, (err) => {
+        if (err) throw err; */
+
+      fs.createReadStream(filePath).pipe(fs.createWriteStream(copyFilePath)); //создаем потоки записи и чтения м записываем данные из папки в копи-папку, используя pipe
+
+    });
+  });
+}
+
+
+try {
+  copyDir();
+  console.log('Все файлы скопированы из папки files в папку files-copy. \nСпасибо за кроссчек!');
+} catch (err) {
+  console.log(err);
+}
+
+
 
